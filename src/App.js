1f468f320ea9cc641e7d0263b6d9a78c23a85fc2
gpiootrek <g.piotreg@gmail.com>
2022-01-31 19:59:19 +0100

    score board not working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { ImCross, ImSpinner11 } from "react-icons/im";
+import { FaDotCircle } from "react-icons/fa";
+import Field from "./components/Field";
 
-function App() {
+const App = () => {
+  const [turn, setTurn] = useState(true);
+  const board = [];
+  const [score, setScore] = useState([
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ]);
+
+  const handleMove = ({ row, col }) => {
+    setTurn((prev) => (prev = !prev));
+    setScore((score[row][col] = turn ? "x" : "o"));
+    console.log(score);
+  };
+
+  const showBoard = () => {
+    console.log(score);
+  };
+
+  for (let i = 0; i < 9; i++) {
+    board.push(
+      <Field
+        index={i}
+        key={i}
+        turn={turn ? 1 : 0}
+        handleMove={handleMove}
+        classes={`board__field`}
+      />
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="board">
+      <div className="board__info">
+        <div>
+          <ImCross className="board__icon board__icon--cross" />
+          <FaDotCircle className="board__icon board__icon--circle" />
+        </div>
+        <div className="board__turn" onClick={showBoard}>
+          {turn ? (
+            <ImCross className="board__icon--cross" />
+          ) : (
+            <FaDotCircle className="board__icon--circle" />
+          )}{" "}
+          Turn
+        </div>
+        <button className="board__button">
+          <ImSpinner11 className="" />
+        </button>
+      </div>
+      {board}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Field.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { ImCross } from "react-icons/im";
+import { FaDotCircle } from "react-icons/fa";
+
+const Field = ({ index, turn, handleMove, classes }) => {
+  const row = Math.floor(index / 3);
+  const col = index % 3;
+  const [icon, setIcon] = useState(null);
+  const [isDisabled, setIsDisabled] = useState(false);
+  const handleFieldClick = () => {
+    handleMove({ row, col });
+    setIcon(
+      turn ? (
+        <ImCross className="board__icon board__icon--cross" />
+      ) : (
+        <FaDotCircle className="board__icon board__icon--circle" />
+      )
+    );
+
+    setIsDisabled(true);
+  };
+
+  return (
+    <button
+      onClick={handleFieldClick}
+      disabled={isDisabled}
+      className={classes}
+    >
+      {icon}
+    </button>
+  );
+};
+
+export default Field;
